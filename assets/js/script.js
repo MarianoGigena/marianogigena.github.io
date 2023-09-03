42ff1a3082efeed5434354c2a604850fec481367
MarianoGigena <95063581+MarianoGigena@users.noreply.github.com>
2023-09-03 16:19:57 -0300

    1er subida

---
+++ assets/js/script.js
@@ -0,0 +1,132 @@
+// Obtén una referencia a la div que deseas utilizar para cambiar el fondo
+const hoverDiv = document.querySelector('.google');
+
+// Define la función para cambiar el fondo
+function cambiarFondo() {
+    // Cambia la imagen de fondo del body
+    document.getElementById("media").style.backgroundImage = 'url("/assets/media/google3d.jpg")';
+}
+
+// Define la función para restaurar el fondo original
+function restaurarFondo() {
+    // Restaura la imagen de fondo original
+    document.getElementById("media").style.backgroundImage = 'none';
+}
+
+// Agrega los listeners de evento para el hover
+hoverDiv.addEventListener('mouseenter', cambiarFondo);
+hoverDiv.addEventListener('mouseleave', restaurarFondo);
+
+// Obtén una referencia a la div que deseas utilizar para cambiar el fondo
+const hoverDiv2 = document.querySelector('.youtube');
+
+// Define la función para cambiar el fondo
+function cambiarFondo2() {
+    // Cambia la imagen de fondo del body
+    document.getElementById("media").style.backgroundImage = 'url("/assets/media/yutubew.jpg")';
+}
+
+// Define la función para restaurar el fondo original
+function restaurarFondo2() {
+    // Restaura la imagen de fondo original
+    document.getElementById("media").style.backgroundImage = 'none';
+}
+
+// Agrega los listeners de evento para el hover
+hoverDiv2.addEventListener('mouseenter', cambiarFondo2);
+hoverDiv2.addEventListener('mouseleave', restaurarFondo2);
+
+// Obtén una referencia a la div que deseas utilizar para cambiar el fondo
+const hoverDiv3 = document.querySelector('.gmail');
+
+// Define la función para cambiar el fondo
+function cambiarFondo3() {
+    // Cambia la imagen de fondo del body
+    document.getElementById("media").style.backgroundImage = 'url("/assets/media/gmailw.webp")';
+}
+
+// Define la función para restaurar el fondo original
+function restaurarFondo3() {
+    // Restaura la imagen de fondo original
+    document.getElementById("media").style.backgroundImage = 'none';
+}
+
+// Agrega los listeners de evento para el hover
+hoverDiv3.addEventListener('mouseenter', cambiarFondo3);
+hoverDiv3.addEventListener('mouseleave', restaurarFondo3);
+
+// Obtén una referencia a la div que deseas utilizar para cambiar el fondo
+const hoverDiv4 = document.querySelector('.gpt');
+
+// Define la función para cambiar el fondo
+function cambiarFondo4() {
+    // Cambia la imagen de fondo del body
+    document.getElementById("media").style.backgroundImage = 'url("/assets/media/gptw.jpg")';
+}
+
+// Define la función para restaurar el fondo original
+function restaurarFondo4() {
+    // Restaura la imagen de fondo original
+    document.getElementById("media").style.backgroundImage = 'none';
+}
+
+// Agrega los listeners de evento para el hover
+hoverDiv4.addEventListener('mouseenter', cambiarFondo4);
+hoverDiv4.addEventListener('mouseleave', restaurarFondo4);
+
+// Obtén una referencia a la div que deseas utilizar para cambiar el fondo
+const hoverDiv5 = document.querySelector('.bard');
+
+// Define la función para cambiar el fondo
+function cambiarFondo5() {
+    // Cambia la imagen de fondo del body
+    document.getElementById("media").style.backgroundImage = 'url("/assets/media/gbardw2.jpg")';
+}
+
+// Define la función para restaurar el fondo original
+function restaurarFondo5() {
+    // Restaura la imagen de fondo original
+    document.getElementById("media").style.backgroundImage = 'none';
+}
+
+// Agrega los listeners de evento para el hover
+hoverDiv5.addEventListener('mouseenter', cambiarFondo5);
+hoverDiv5.addEventListener('mouseleave', restaurarFondo5);
+
+// Obtén una referencia a la div que deseas utilizar para cambiar el fondo
+const hoverDiv6 = document.querySelector('.whatsapp');
+
+// Define la función para cambiar el fondo
+function cambiarFondo6() {
+    // Cambia la imagen de fondo del body
+    document.getElementById("media").style.backgroundImage = 'url("/assets/media/wsw.webp")';
+}
+
+// Define la función para restaurar el fondo original
+function restaurarFondo6() {
+    // Restaura la imagen de fondo original
+    document.getElementById("media").style.backgroundImage = 'none';
+}
+
+// Agrega los listeners de evento para el hover
+hoverDiv6.addEventListener('mouseenter', cambiarFondo6);
+hoverDiv6.addEventListener('mouseleave', restaurarFondo6);
+
+// Obtén una referencia a la div que deseas utilizar para cambiar el fondo
+const hoverDiv7 = document.querySelector('.cdc');
+
+// Define la función para cambiar el fondo
+function cambiarFondo7() {
+    // Cambia la imagen de fondo del body
+    document.getElementById("media").style.backgroundImage = 'url("/assets/media/cdc.png")';
+}
+
+// Define la función para restaurar el fondo original
+function restaurarFondo7() {
+    // Restaura la imagen de fondo original
+    document.getElementById("media").style.backgroundImage = 'none';
+}
+
+// Agrega los listeners de evento para el hover
+hoverDiv7.addEventListener('mouseenter', cambiarFondo7);
+hoverDiv7.addEventListener('mouseleave', restaurarFondo7);
